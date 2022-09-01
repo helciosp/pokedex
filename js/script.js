053c625ea5a11727c742f1b18f82ac3f49f5368a
helciosp <helciosp43@gmail.com>
2022-09-01 20:29:56 -0300

    Link para o github e modo dark

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ $("#load").on("click", function() {
     buscarPokemons(qtPokemon, limint);
 });
 
+$("#template").on("click", function() {
+    $("main").toggleClass("dark")
+    $(".button").toggleClass("dark")
+    $(".pokemon").toggleClass("dark")
+    $("main").hasClass("dark") ? $("#template").text("Dark") : $("#template").text("Light")
+})
+
 function buscarPokemons(x, y) {
     $.get(`https://pokeapi.co/api/v2/pokemon?limit=${y}&offset=${x}`, (data) => {
         criarElementos(data.results);
@@ -15,8 +22,9 @@ function buscarPokemons(x, y) {
 }
 
 function criarElementos(pokedex) {
+    let dark = $("main").hasClass("dark")
     pokedex.map((item) => {
-        $("#pokedex-div").append(`<article class="pokemon" id="${item.name}"></article>`);
+        $("#pokedex-div").append(`<article class="pokemon ${dark ? "dark" : null}" id="${item.name}"></article>`);
         buscarInformacoes(item.url);
     });
 }
@@ -26,12 +34,14 @@ function buscarInformacoes(url) {
         const types = data.types.map(item => `<li class="type ${item.type.name}">${item.type.name}</li>`);
 
         $(`#${data.name}`).append(
-            `<p class="id">#${data.id}</p>
-            <img class="imagem" src=${data.sprites.front_default} title="PokeAPI" />
-            <h3 class="nome">${data.name}</h3>
-            <ul class="types">
-                ${types.join("")}
-            </ul>`
+            `<div>
+                <p class="id">#${data.id}</p>
+                <img class="imagem" src=${data.sprites.front_default} title="PokeAPI" />
+                <h3 class="nome">${data.name}</h3>
+                <ul class="list-style text-center">
+                    ${types.join("")}
+                </ul>
+            </div>`
         );
     });
 }
